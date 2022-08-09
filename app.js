8ea3032ff9813c5e7c36d37a76014ffc7a155360
Lawson <lawsonmcphetridge2020@gmail.com>
2022-08-08 17:33:49 -0700

    got website working sorry I forgot tocommit more

--- app.js
+++ app.js
@@ -3,6 +3,18 @@ import { renderDogCard } from './render-utils.js';
 
 const dogListContainer = document.getElementById('dog-list-container');
 
-// on load
-// fetch all dogs
-// render and append all dog cards to the container
+// on load  async/await
+// fetch all dogs//  defined in the fetch utils
+// render and append all dog cards to the container render utils
+
+async function loadData() {
+    const dogs = await getDogs();
+
+    for (let dog of dogs) {
+        const dogContainer = renderDogCard(dog);
+    
+        dogListContainer.append(dogContainer);
+    }
+}
+
+loadData();
--- detail/detail.js
+++ detail/detail.js
@@ -4,6 +4,14 @@ import { renderDogDetail } from '../render-utils.js';
 const dogDetailContainer = document.getElementById('dog-detail-container');
 
 // on load
+const params = new URLSearchParams(window.location.search);
+
+async function loadData() {
+    const data = await getDog(params.get('id'));
+    const dogDiv = renderDogDetail(data);
+    dogDetailContainer.append(dogDiv);
+}
+loadData();
 // get the id from URL
 // use the id to fetch the dog
 // render and append this dog's details to the container
